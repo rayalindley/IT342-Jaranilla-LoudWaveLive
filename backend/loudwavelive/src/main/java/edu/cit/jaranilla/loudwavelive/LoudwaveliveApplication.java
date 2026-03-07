9d29c31215c60a84e3099cf643397d92ebf3580a
Raysie <107735270+rayalindley@users.noreply.github.com>
2026-03-07 22:03:12 +0800

    Initial project structure

---
+++ backend/loudwavelive/src/main/java/edu/cit/jaranilla/loudwavelive/LoudwaveliveApplication.java
@@ -0,0 +1,13 @@
+package edu.cit.jaranilla.loudwavelive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoudwaveliveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoudwaveliveApplication.class, args);
+	}
+
+}
---
+++ backend/loudwavelive/src/test/java/edu/cit/jaranilla/loudwavelive/LoudwaveliveApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cit.jaranilla.loudwavelive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoudwaveliveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
