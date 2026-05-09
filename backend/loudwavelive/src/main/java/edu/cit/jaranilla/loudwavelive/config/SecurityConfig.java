76d9a1aacfa6bac3fa45d5dcea86feeb35dc8187
Raysie <107735270+rayalindley@users.noreply.github.com>
2026-05-09 22:38:52 +0800

    updated permit config

--- backend/loudwavelive/src/main/java/edu/cit/jaranilla/loudwavelive/config/SecurityConfig.java
+++ backend/loudwavelive/src/main/java/edu/cit/jaranilla/loudwavelive/config/SecurityConfig.java
@@ -44,12 +44,9 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/v1/auth/**").permitAll()
-<<<<<<< Updated upstream
-=======
                 .requestMatchers("/api/events/**").permitAll()
                 .requestMatchers("/api/tickets/**").permitAll()
                 .requestMatchers("/api/payment/**").permitAll()
->>>>>>> Stashed changes
                 .anyRequest().authenticated()
             );
 
